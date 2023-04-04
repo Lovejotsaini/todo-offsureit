f39b1bb3b7a8d92a52585ce431aa7aec07d6cf64
lovejot.singh <lovejot.singh@kochartech.com>
2023-04-05 00:20:57 +0530

    final setup

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from '../slices/todoSlice';
 
+//store
 export const store = configureStore({
   reducer: {
     todo: todoReducer,
